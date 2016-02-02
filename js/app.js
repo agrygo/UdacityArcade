73fa386532b00791a7bbfdf473ffdc76895dacec
agrygo <andreagrygo@gmail.com>
2016-02-02 14:54:24 -0700

    show player and enemy

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = 10;
+    this.y = 50;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -25,13 +26,16 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function(x, y) {
-    this.sprite = 'images/char-boy.png';
+    this.sprite = 'images/char-horn-girl.png';
+    this.x = 200;
+    this.y = 435;
 };
 Player.prototype.update = function(dt){
 
 };
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
 };
 Player.prototype.handleInput = function() {
 
@@ -41,8 +45,8 @@ Player.prototype.handleInput = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy(10,100), new Enemy(10, 200)];
-var player = new Player(225, 600);
+var allEnemies = [new Enemy(250,250), new Enemy(350, 350)];
+var player = new Player(225, 225);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
