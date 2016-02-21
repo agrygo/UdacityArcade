2ba8461221a07d28573d62e9356e9008f4a7568a
agrygo <andreagrygo@gmail.com>
2016-02-21 09:44:26 -0700

    Add collision detection

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.speed = Math.floor(Math.random() * 250 + 1);
+    this.speed = Math.floor(Math.random() * 225);
     this.x = x;
     this.y = y;
     // The image/sprite for our enemies, this uses
@@ -16,8 +16,6 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    //this.x = this.x + (dt * 50);
-
     this.x += dt * this.speed;
     if (this.x >= 505) {
         this.x = 0;
@@ -44,6 +42,11 @@ Player.prototype.update = function(){
 	} else if (this.x > 410 || this.x < -10){
 		this.x = 200
 	}
+	//check for collisions
+	if (checkCollisions(allEnemies) === true) {
+		this.x = 200;
+		this.y = 435;
+	}
 };
 
 Player.prototype.render = function() {
@@ -63,20 +66,30 @@ Player.prototype.handleInput = function(keyCode) {
 	    console.log("x " + this.x + "y" + this.y);
 };
 
+//check for Enemy - Player collision
+//101 x 171 img size
+function checkCollisions (enemy) {
+	for (var i=0; i < allEnemies.length; i++){
+		if (allEnemies[i].x < player.x + 50 &&
+			allEnemies[i].x + 85 > player.x &&
+			allEnemies[i].y < player.y + 85 &&
+			50 + allEnemies[i].y > player.y){
+			console.log("collision detected");
+			return true;
+		}
+	}
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy(10,60), new Enemy(10, 200), new Enemy(150, 125)];
+var allEnemies = [new Enemy(10, 60), new Enemy(10, 200), new Enemy(150, 125)];
 var player = new Player(200, 435);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
-
-
-
         37: 'left',
         38: 'up',
         39: 'right',
